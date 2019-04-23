ee91f85c1b336b6cff66f5aa41730f7f0c3fb2c3
Chandan Jain <c.jainn@ymail.com>
2019-04-23 17:59:41 +0530

    changed users source

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,40 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 interface user {
-  id: number;
-  name: string;
+	id: number;
+	name: string;
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  users;
-  searchedUser = '';
-  selectedUser: user;
+	users;
+	searchedUser = '';
+	selectedUser: user;
 
-  constructor(private httpClient: HttpClient) {}
-  ngOnInit() {
-    this.httpClient
-      .get(
-        'https://gorest.co.in/public-api/users?_format=json&access-token=IHV_k_ry6RjB1Pns90WDZKKGVih42hXSeEp2'
-      )
-      .subscribe((res: any) => {
-        this.users = res.result;
-      });
-  }
+	constructor(private httpClient: HttpClient) {}
+	ngOnInit() {
+		this.httpClient.get('../assets/users.json').subscribe((res: any) => {
+			this.users = res.result;
+		});
+	}
 
-  showData(name: string) {
-    // this.users.forEach((user, index) => {
-    //   if (user.id === id) {
-    //     this.selectedUser = this.users[index];
-    //   }
-    // });
-    this.selectedUser = this.users.find(item => item.name === name);
-  }
-  clear() {
-    this.selectedUser = null;
-  }
+	showData(name: string) {
+		// this.users.forEach((user, index) => {
+		//   if (user.id === id) {
+		//     this.selectedUser = this.users[index];
+		//   }
+		// });
+		this.selectedUser = this.users.find(item => item.name === name);
+	}
+	clear() {
+		this.selectedUser = null;
+	}
 }
